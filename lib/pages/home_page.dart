fcff0c8495f262812efcb4e80a6d5d533cfe5eb6
221110019 <221110019@students.mikroskil.ac.id>
2024-06-28 11:47:31 +0700

    update UI, validasi method, dan membagi section

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,20 +6,20 @@ import 'package:speech_to_text/speech_to_text.dart';
 import 'package:translator/translator.dart';
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String dropDownButtonValue = 'Inggris';
-  SpeechToText _speechToText = SpeechToText();
+  final GoogleTranslator translator = GoogleTranslator();
+  final SpeechToText _speechToText = SpeechToText();
+  // String dropDownButtonValue = 'Inggris';
   bool _speechEnabled = false;
   String _lastWords = '';
   String resultSpeech = '';
   String resultTranslate = '';
-  final translator = GoogleTranslator();
   String inputLanguage = 'en';
   String outputLanguage = 'fr';
   Future<void> translateText() async {
@@ -36,6 +36,13 @@ class _MyHomePageState extends State<MyHomePage> {
     await flutterTts.setLanguage(ttsLanguage);
     await flutterTts.setPitch(1);
     await flutterTts.speak(text);
+  }
+
+  bool _isTranslateVoice = false;
+  void _translateVoice() {
+    setState(() {
+      _isTranslateVoice = !_isTranslateVoice;
+    });
   }
 
   @override
@@ -63,7 +70,9 @@ class _MyHomePageState extends State<MyHomePage> {
   void _stopListening() async {
     resultSpeech = _lastWords;
     await _speechToText.stop();
-    setState(() {});
+    setState(() {
+      translateText();
+    });
   }
 
   /// This is the callback that the SpeechToText plugin calls when
@@ -79,158 +88,253 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.only(top: 70, left: 50, right: 50),
+          padding: const EdgeInsets.only(top: 70, left: 50, right: 50),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  'Speech Recognition:',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Icon(
-                Icons.mic,
-                size: 60,
-                color: Colors.deepPurpleAccent,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 150,
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.black)),
-                padding: EdgeInsets.all(16),
-                child: SingleChildScrollView(
-                  child: Text(
-                    // If listening is active show the recognized words
-                    _speechToText.isListening
-                        ? '$_lastWords'
-                        : resultSpeech != ''
-                            ? '$resultSpeech'
-                            // If listening isn't active but could be tell the user
-                            // how to start it, otherwise indicate that speech
-                            // recognition is not yet ready or not supported on
-                            // the target device
-                            : _speechEnabled
-                                ? 'Tap the microphone to start listening...'
-                                : 'Speech not available',
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  DropdownButton<String>(
-                    value: inputLanguage,
-                    onChanged: (newValue) {
-                      setState(() {
-                        inputLanguage = newValue!;
-                      });
-                    },
-                    items: <String>['en', 'fr', 'es', 'de', 'id']
-                        .map<DropdownMenuItem<String>>(
-                            (String value) => DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(
-                                    value,
-                                  ),
-                                ))
-                        .toList(),
-                  ),
-                  Icon(Icons.arrow_forward_rounded),
-                  DropdownButton<String>(
-                    value: outputLanguage,
-                    onChanged: (newValue) {
-                      setState(() {
-                        outputLanguage = newValue!;
-                        if (outputLanguage == 'en') {
-                          ttsLanguage = 'en-US';
-                        } else if (outputLanguage == "fr") {
-                          ttsLanguage = "fr-FR";
-                        } else if (outputLanguage == "es") {
-                          ttsLanguage = "es-ES";
-                        } else if (outputLanguage == "de") {
-                          ttsLanguage = "de-DE";
-                        } else {
-                          ttsLanguage = "id-ID";
-                        }
-                      });
-                      print(ttsLanguage);
-                    },
-                    items: <String>['en', 'fr', 'es', 'de', 'id']
-                        .map<DropdownMenuItem<String>>(
-                            (String value) => DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(
-                                    value,
-                                  ),
-                                ))
-                        .toList(),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.deepPurpleAccent),
-                  onPressed: () {
-                    translateText();
-                  },
-                  child: Text(
-                    'Translate',
-                  )),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 150,
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.black)),
-                padding: EdgeInsets.all(16),
-                child: SingleChildScrollView(
-                  child: Text(resultTranslate),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              IconButton(
-                  onPressed: () {
-                    speak(resultTranslate);
-                  },
-                  icon: Icon(Icons.volume_up))
+            children: [
+              const NamaAplikasi(),
+              bagianInputSpeech(context),
+              bagianSelectLanguage(),
+              bagianTranslateOutput(context)
             ],
           ),
         ),
       ),
-      floatingActionButton: AvatarGlow(
-        animate: _speechToText.isListening,
-        glowColor: Colors.grey,
-        child: FloatingActionButton(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
-          onPressed:
-              // If not yet listening for speech start, otherwise stop
-              _speechToText.isNotListening ? _startListening : _stopListening,
-          tooltip: 'Listen',
-          child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
+      // floatingActionButton: AvatarGlow(
+      //   animate: _speechToText.isListening,
+      //   glowColor: Colors.grey,
+      //   child: FloatingActionButton(
+      //     shape:
+      //         RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+      //     onPressed:
+      //         // If not yet listening for speech start, otherwise stop
+      //         _speechToText.isNotListening ? _startListening : _stopListening,
+      //     tooltip: 'Listen',
+      //     child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
+      //   ),
+      // ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+
+  Column bagianInputSpeech(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 250,
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                AvatarGlow(
+                  animate: _speechToText.isListening,
+                  glowColor: Colors.grey,
+                  child: FloatingActionButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100)),
+                    onPressed:
+                        // If not yet listening for speech start, otherwise stop
+                        _speechToText.isNotListening
+                            ? _startListening
+                            : _stopListening,
+                    tooltip: 'Listen',
+                    child: Icon(_speechToText.isNotListening
+                        ? Icons.mic_off
+                        : Icons.mic),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  // If listening is active show the recognized words
+                  _speechToText.isListening
+                      ? _lastWords
+                      : resultSpeech != ''
+                          ? resultSpeech
+                          // If listening isn't active but could be tell the user
+                          // how to start it, otherwise indicate that speech
+                          // recognition is not yet ready or not supported on
+                          // the target device
+                          : _speechEnabled
+                              ? 'Tap the microphone to start listening...'
+                              : 'Speech not available',
+                  style: TextStyle(
+                      color: resultSpeech.isEmpty ? Colors.grey : Colors.black),
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        const SizedBox(
+          height: 20,
+        ),
+      ],
+    );
+  }
+
+  Row bagianSelectLanguage() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        DropdownButton<String>(
+          value: inputLanguage,
+          onChanged: (newValue) {
+            setState(() {
+              inputLanguage = newValue!;
+            });
+          },
+          items: <String>['en', 'fr', 'es', 'de', 'id']
+              .map<DropdownMenuItem<String>>(
+                  (String value) => DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value,
+                        ),
+                      ))
+              .toList(),
+        ),
+        const Icon(Icons.arrow_forward_rounded),
+        DropdownButton<String>(
+          value: outputLanguage,
+          onChanged: (newValue) {
+            setState(() {
+              outputLanguage = newValue!;
+              if (outputLanguage == 'en') {
+                ttsLanguage = 'en-US';
+              }
+              if (outputLanguage == "fr") {
+                ttsLanguage = "fr-FR";
+              }
+              if (outputLanguage == "es") {
+                ttsLanguage = "es-ES";
+              }
+              if (outputLanguage == "de") {
+                ttsLanguage = "de-DE";
+              }
+              if (outputLanguage == "id") {
+                ttsLanguage = "id-ID";
+              }
+            });
+            translateText();
+            debugPrint(ttsLanguage);
+          },
+          items: <String>['en', 'fr', 'es', 'de', 'id']
+              .map<DropdownMenuItem<String>>(
+                  (String value) => DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value,
+                        ),
+                      ))
+              .toList(),
+        ),
+      ],
+    );
+  }
+
+  Column bagianTranslateOutput(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 20,
+        ),
+        // ElevatedButton(
+        //     style: ElevatedButton.styleFrom(
+        //         foregroundColor: Colors.deepPurpleAccent),
+        //     onPressed: () {
+        //       translateText();
+        //     },
+        //     child: const Text(
+        //       'Translate',
+        //     )),
+        // const SizedBox(
+        //   height: 20,
+        // ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 250,
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: resultTranslate.isEmpty
+                ? const Column(children: [
+                    Text(
+                      "No speech detected",
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ])
+                : Column(
+                    children: [
+                      AvatarGlow(
+                        animate: _isTranslateVoice,
+                        glowColor: Colors.grey,
+                        child: FloatingActionButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(100)),
+                          onPressed: () async {
+                            _translateVoice();
+                            await speak(resultTranslate);
+                            Future.delayed(
+                                const Duration(seconds: 3), _translateVoice);
+                          },
+                          tooltip: 'Speak',
+                          child: const Icon(Icons.volume_up),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        resultTranslate,
+                        style: const TextStyle(color: Colors.black),
+                      )
+                    ],
+                  ),
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+      ],
     );
   }
 }
-// _speechToText.isNotListening ? _startListening : _stopListening,
+
+class NamaAplikasi extends StatelessWidget {
+  const NamaAplikasi({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Text(
+            'SPEECH RECOGNITION',
+            style: TextStyle(
+              shadows: const [Shadow(blurRadius: 20, color: Colors.grey)],
+              fontWeight: FontWeight.bold,
+              color: Colors.deepPurpleAccent.withOpacity(0.6),
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+        Icon(
+          Icons.mic,
+          size: 20,
+          shadows: const [Shadow(blurRadius: 20, color: Colors.grey)],
+          color: Colors.deepPurpleAccent.withOpacity(0.6),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+      ],
+    );
+  }
+}
